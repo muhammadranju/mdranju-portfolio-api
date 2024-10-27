2dd174d033408614f47838025ebac8e1297f00e9
Muhammad Ranju <mdranju23@gmail.com>
2024-10-27 10:57:56 +0600

    feat: Add health check endpoint and remove src folder

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World" });
 });
-
+app.get("/health", (req, res) => {
+  res.status(200).json({ message: "Healthy" });
+});
 createServer.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
