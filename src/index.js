d03b7ae7ca723121646c9100424b91fcbcaef3b0
Muhammad Ranju <mdranju23@gmail.com>
2024-10-27 10:45:27 +0600

    feat: Initial setup for portfolio projects API

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const createServer = http.createServer(app);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello World" });
+});
+
+createServer.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
